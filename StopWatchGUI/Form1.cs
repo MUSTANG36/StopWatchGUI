3e0c14865aa6a6973c40324fa4fed6acce564834
Ricardo <ochor01@pfw.edu>
2021-03-31 13:23:37 -0400

    Using StopWatch library

--- StopWatchGUI/Form1.cs
+++ StopWatchGUI/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics; //For stopwatch 
 
 namespace StopWatchGUI
 {
     public partial class Form1 : Form
     {
+
+        private Stopwatch stopWatch;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e) {
+            stopWatch = new Stopwatch();
+        
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopWatch.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stopWatch.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stopWatch.Restart();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = string.Format("{0:hh\\:mm\\:ss}", stopWatch.Elapsed);
+        }
+
+       
     }
 }
